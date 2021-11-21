5dc2dea604b0a0686b333a6fc3ed787e6ce2c21d
Madhubala <madhu.a1993@gmail.com>
2021-11-21 15:02:52 +0530

    color bg

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-let loadnum=()=>{
-    let n1=Math.floor(Math.random()*10);
-    let n2=Math.floor(Math.random()*10);
-    console.log(n1, n2);
-    document.querySelector("#num1").value=n1;
-    document.querySelector("#num2").value=n2;
-}
+var colors = ['red', 'blue', 'green', 'grey'];
 
-let ans=()=>{
-    let n1= Number(document.querySelector("#num1").value);
-    let n2= Number(document.querySelector("#num2").value);
-    
-    let sum = n1+n2;
-    let answer = document.querySelector("#output").value;
-    console.log(answer);
-    if(answer==sum){
-        alert("Correct");
-    }
-    else
-        alert("Check. Correct ans is " +sum); 
-        loadnum();  
+let changecolor=()=>{
+    var randColor = colors[Math.floor(Math.random()*colors.length)];
+document.body.style.backgroundColor = randColor;
 }
