e24fa29c742eac77400bbb99e290a48ba887c4b3
Madhubala <madhu.a1993@gmail.com>
2021-11-21 13:06:53 +0530

    Files added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-history1
+var colors = ['red', 'blue', 'green', 'grey'];
+
+
+let changecolor=()=>{
+    var randColor = colors[Math.floor(Math.random()*colors.length)];
+document.body.style.backgroundColor = randColor;
+}
