96f62fc06c1dd30d928f06c8d1755aeeef703cc5
Madhubala <madhu.a1993@gmail.com>
2021-11-23 01:56:36 +0530

    DOM-Manipulation-with-Forms

--- script.js
+++ script.js
@@ -1,115 +1,41 @@
-const time = 1000;
-
-function timer1(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "10";
-    console.log("10");
-    callback();
-  },time);
+function clearTable(){
+  document.getElementById("firstname").value ="";
+document.getElementById("lastname").value ="";
+document.getElementById("address").value ="";
+document.getElementById("pincode").value ="";
+document.getElementById("gender").value ="";
+  document.getElementById("food1").value = "";
+  document.getElementById("food2").value ="";
+document.getElementById("state").value ="";
+document.getElementById("country").value ="";
 }
 
-function timer2(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "9";
-    console.log("9");
-    callback();
-  }, time);
-}
-
-function timer3(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "8";
-    console.log("8");
-    callback();
-  },time);
-}
-
-function timer4(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "7";
-    console.log("7");
-    callback();
-  },time);
-}
-
-function timer5(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "6";
-    console.log("6");
-    callback();
-  },time);
-}
-
-function timer6(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "5";
-    console.log("5");
-    callback();
-  },time);
-}
-
-function timer7(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "4";
-    console.log("4");
-    callback();
-  },time);
-}
-
-function timer8(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "3";
-    console.log("3");
-    callback();
-  },time);
-}
-function timer9(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "2";
-    console.log("2");
-    callback();
-  },time);
-}
-function timer10(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "1";
-    console.log("1");
-    callback();
-  },time);
-}
-function timer11(callback) {
-  setTimeout(() => {
-    document.getElementById("count").innerHTML = "0";
-    console.log("0");
-    callback();
-  },time);
-}
-// callback hell
-let countDown = () => {
-  timer1(() => {
-    timer2(() => {
-      timer3(() => {
-        timer4(() => {
-          timer5(() => {
-            timer6(() => {
-              timer7(() => {
-                timer8(() => {
-                  timer9(() => {
-                    timer10(() => {
-                      timer11(() => {
-                        setTimeout(() => {
-                          document.getElementById("count").innerHTML = "HAPPY INDEPENDENCE DAY";
-                          console.log("HAPPY INDEPENDENCE DAY");
-                        }, time);
-                      });
-                    });
-                  });
-                });
-              });
-            });
-          });
-        });
-      });
-    });
-  });
-}
+let data = document.querySelector("#addDatas");
+let tableRow = "";
+data.addEventListener("click", function () {
+  let firstname = document.getElementById("firstname").value;
+  let lastname = document.getElementById("lastname").value;
+  let address = document.getElementById("address").value;
+  let pincode = document.getElementById("pincode").value;
+  let gender = document.getElementById("gender").value;
+  let food1 = document.getElementById("food1").value;
+  let food2 = document.getElementById("food2").value;
+  let state = document.getElementById("state").value;
+  let country = document.getElementById("country").value;
+  
+  let tableBody=document.querySelector("#tablebody");
+        tableRow+=`<tr>
+        <td>${firstname}</td>
+        <td>${lastname}</td>
+        <td>${address}</td>
+        <td>${pincode}</td>
+        <td>${gender}</td>
+        <td>${food1} , ${food2}</td>
+        <td>${state}</td>
+        <td>${country}</td>
+        </tr>
+        `;
+        console.log(tableRow);
+  tableBody.innerHTML = tableRow;
+  setTimeout(clearTable, 100);
+});
